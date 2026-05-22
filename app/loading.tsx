e603ec68b025f44de5c3c2ca005159ed48d59281
Tamanna Rath <tamannaarath@gmail.com>
2026-05-22 12:01:37 +0530

    Replace Spinner with FullScreenSpinner in Loading component

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,5 +1,5 @@
-import { Spinner } from "../components/Spinner";
+import { FullScreenSpinner } from "../components/FullScreenSpinner";
 
 export default function Loading() {
-    return <Spinner />;
+    return <FullScreenSpinner />;
 }
